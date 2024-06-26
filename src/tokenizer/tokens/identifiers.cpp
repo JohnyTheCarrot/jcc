ea625814c7da9be487b1504f954d06eb9013b592
JohnyTheCarrot <tuurmartens4@gmail.com>
2024-06-26 14:26:12 +0200

    refactor(Tokenizer): split tokenization into multiple files

---
+++ src/tokenizer/tokens/identifiers.cpp
@@ -0,0 +1,55 @@
+#include "identifiers.h"
+#include "misc/Diagnosis.h"
+
+namespace jcc::tokenizer::identifiers {
+	[[nodiscard]]
+	std::optional<SpanMarker> CollectRestOfIdentifier(CharIter &charIter, std::string &identifier) {
+		std::optional<SpanMarker> lastSpanMarker{};
+
+		for (CharIter const untilNewline{'\n'}; charIter != untilNewline; ++charIter) {
+			auto const [spanMarker, character, isSentinel]{*charIter};
+
+			if (!Identifier::IsValidChar(character))
+				break;
+
+			identifier.push_back(character);
+			lastSpanMarker = spanMarker;
+		}
+
+		return lastSpanMarker;
+	}
+
+	Token Tokenize(CharIter &charIter, IdentifierTokenStart const &tokenStart) {
+		if (charIter == CharIter::end()) {
+			Span span{
+			        charIter.GetFileName(), tokenStart.m_Start, charIter.GetSentinel().m_LastSpanMarker,
+			        charIter.GetInput()->tellg(), charIter.GetInput()
+			};
+
+			return Token{.m_Value = Identifier{tokenStart.m_Identifier}, .m_Span = std::move(span)};
+		}
+
+		std::string identifier{};
+		auto const &[partialIdentifier, identifierStart, identifierStartPos]{tokenStart};
+
+		Span span{
+		        charIter.GetFileName(), identifierStart, identifierStart, charIter.GetInput()->tellg(),
+		        charIter.GetInput()
+		};
+
+		identifier.reserve(partialIdentifier.length());
+		std::ranges::move(partialIdentifier, std::back_inserter(identifier));
+
+		if (auto const optSpanMarker{CollectRestOfIdentifier(charIter, identifier)}; optSpanMarker.has_value()) {
+			span.m_End = optSpanMarker.value();
+		}
+		// check if identifier contains a backslash
+		if (identifier.find('\\') != std::string::npos)
+			// TODO: Universal character names
+			throw FatalCompilerError{Diagnosis::Kind::TODO, std::move(span)};
+
+		Identifier identToken{std::move(identifier)};
+
+		return Token{.m_Value = std::move(identToken), .m_Span = std::move(span)};
+	}
+}// namespace jcc::tokenizer::identifier_tokenizer
